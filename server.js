ca64f0305797a17c22ff38573b7834c445d49e94
Jim Tsai <jimtsai@Jims-MacBook-Pro.local>
2019-02-13 16:51:54 -0500

    add microservice

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,9 +19,19 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  if(!req.params.date_string){
+    res.json({
+      "unix": Date.now(),
+      "utc": (new Date()).toUTCString()
+    })
+  } else {
+    res.json({
+      "unix": new Date(req.params.date_string).getTime(),
+      "utc": (new Date(req.params.date_string)).toUTCString()
+    })
+  }
 });
 
 
